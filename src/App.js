8415090b4e59e5fe6b4c2c04c30711f4f996d251
Mikhel Róbert <robert.mikhel@gmail.com>
2024-05-09 15:06:35 +0200

    Create React project and install Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container className='container justify'>
+        <h1>React Todolist</h1>
+        <Container className='inputBox'>
+          <h5>Add task</h5>
+          <Row>
+            <Col><input className='inputFields' type="text" placeholder='Describe task' /></Col>
+            <Col><Button variant="outline-secondary">Save</Button>{' '}</Col>
+          </Row>
+
+        </Container>
+
+
+      </Container>
     </div>
   );
 }
