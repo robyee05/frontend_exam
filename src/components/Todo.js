99deba2eae3ccd99cc8b24b279c9a13051a1d0ee
Mikhel Róbert <robert.mikhel@gmail.com>
2024-05-10 13:36:35 +0200

    Adjust the form founctionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo, onChange }) => {
+    const [checked, setChecked] = useState(false);
+
+    const handleCheckboxChange = () => {
+        setChecked(!checked);
+        onChange(); // Átadjuk az állapotváltozást a szülő komponensnek
+    };
+
     return (
         <div className='todoContainer'>
-
             <div>
-                <input type="checkbox" name="" id="" />
-                {todo}
+                <label>
+                    <input type="checkbox" name="" id="listed" checked={checked} onChange={handleCheckboxChange} />
+                    {todo}
+                </label>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Todo
+export default Todo;
